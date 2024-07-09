bfea1896d62765697c4b78c8fe40109ece9d0106
Marcelo William <marcelo.willian@live.com>
2024-07-09 17:05:19 -0300

    refactor: usuarios e transacao

--- domain/transacao.py
+++ domain/transacao.py
@@ -1,8 +1,32 @@
 class Transacao:
-    def __init__(self, valor, data, origem, destino, status, id_transacao = None):
+    def __init__(self, valor, data, origem, destino, status, id_transacao=None):
         self._id = id_transacao
         self._valor = valor
         self._data = data
         self._origem = origem
-        self.destino = destino
-        self.status = status
+        self._destino = destino
+        self._status = status
+
+    @property
+    def id_transacao(self):
+        return self._id
+
+    @property
+    def valor(self):
+        return self._valor
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def origem(self):
+        return self._origem
+
+    @property
+    def destino(self):
+        return self._destino
+
+    @property
+    def status(self):
+        return self._status
--- domain/usuario.py
+++ domain/usuario.py
@@ -1,10 +1,32 @@
 class Usuario:
-    def __init__(self, nome, agencia, conta, saldo, chaves, id_usuario = None):
+    def __init__(self, nome, agencia, conta, saldo, chaves, id_usuario=None):
         self._id = id_usuario
         self._nome = nome
         self._agencia = agencia
         self._conta = conta
-        self.saldo = saldo
+        self._saldo = saldo
         self._chaves = chaves
 
-    
+    @property
+    def id_usuario(self):
+        return self._id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def agencia(self):
+        return self._agencia
+
+    @property
+    def conta(self):
+        return self._conta
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @property
+    def chave(self):
+        return self._chaves

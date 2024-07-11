0cadea257e9360921eb6d81b8f2db1a131bc5120
Marcelo William <marcelo.willian@live.com>
2024-07-11 15:47:58 -0300

    refactor: mudança de diretório

--- db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-config = {"host": "localhost", "user": "root", "password": "1234", "database": "db_pix"}
-conexao = mysql.connector.connect(**config)
-
-
-def cursor():
-    cursor = conexao.cursor()

1dc6277121ebc97a9fbd30358582d0c3e4cc5503
Marcelo William <marcelo.willian@live.com>
2024-07-11 15:49:30 -0300

    refactor: incluido os atibutos cpf, telefone e email na classe usuário

--- domain/usuario.py
+++ domain/usuario.py
@@ -1,7 +1,10 @@
 class Usuario:
-    def __init__(self, nome, agencia, conta, saldo, chaves, id_usuario=None):
+    def __init__(self, nome, cpf, telefone, email, agencia, conta, saldo, chaves, id_usuario=None):
         self._id = id_usuario
         self._nome = nome
+        self._cpf = cpf
+        self._telefone = telefone
+        self._email = email
         self._agencia = agencia
         self._conta = conta
         self._saldo = saldo
@@ -14,14 +17,26 @@ class Usuario:
     @property
     def nome(self):
         return self._nome
+    
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @property
+    def telefone(self):
+        return self._telefone
 
     @property
-    def agencia(self):
-        return self._agencia
+    def email(self):
+        return self._email
 
     @property
     def conta(self):
         return self._conta
+    
+    @property
+    def agencia(self):
+        return self._agencia
 
     @property
     def saldo(self):

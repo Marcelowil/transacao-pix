3d2d86a6bea1fb2f2845ab9fb54309c8bac5b567
Marcelo William <marcelo.willian@live.com>
2024-07-27 17:33:58 -0300

    feat: função gerar_usuario

--- services/db.py
+++ services/db.py
@@ -1,8 +1,8 @@
 import mysql.connector
 import sys
 import os
-from validate_docbr import CPF
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from services.formatar_chaves import formatar_chaves
 from domain.usuario import Usuario
 from services.BrasilAPI import buscar_nome_banco
 
@@ -25,12 +25,8 @@ def buscar_usuario_chave(cpf, agencia):
     cursor.execute("SELECT tipo, valor FROM tb_chaves_pix WHERE usuario_id = %s;", (usuario[0],))
     chaves = cursor.fetchall()
 
-    lista_chaves={}
-    for row in chaves:
-        lista_chaves[row[0]] = row[1]
-
-    usuario = Usuario(usuario[1], usuario[2], usuario[3], usuario[4], usuario[5], usuario[6], usuario[7], lista_chaves, usuario[0])
-    return usuario
+    return gerar_usuario(usuario, chaves)
+   
 
 def retornar_saldo(id):
     cursor.execute("SELECT saldo FROM tb_usuarios WHERE id = %s;", (id, ))
@@ -60,22 +56,14 @@ def cadastrar_chaves_db(tipo, id):
         return f"{valor} já cadastrado como chave pix!"
     
 def identificar_destinatario(valor):
-    cursor.execute("SELECT u.nome, u.agencia, p.tipo, p.valor FROM tb_usuarios u LEFT JOIN tb_chaves_pix p ON u.id = p.usuario_id WHERE p.valor = %s;", (valor, ))
+    cursor.execute("SELECT u.cpf, u.agencia, p.tipo FROM tb_usuarios u LEFT JOIN tb_chaves_pix p ON u.id = p.usuario_id WHERE p.valor = %s;", (valor, ))
     destinatario = cursor.fetchone();
+    return buscar_usuario_chave(destinatario[0], destinatario[1]), destinatario[2]
 
-    return destinatario
+def gerar_usuario(usuario, chaves):
+    lista_chaves={}
+    for row in chaves:
+        lista_chaves[row[0]] = row[1]
 
-
-def formatar_chaves(tipo, chave):
-    if len(chave) == 11:
-        if tipo == 'cpf':
-            chave = CPF().mask(chave)
-        else:
-            ddd = chave[:2]
-            primeira_parte = chave[2:7]
-            segunda_parte = chave[7:]
-            chave = f"({ddd}) {primeira_parte}-{segunda_parte}"
-    elif '@' in chave and tipo == 'email':
-        return chave
-    
-    return chave
+    usuario = Usuario(usuario[1], usuario[2], usuario[3], usuario[4], usuario[5], usuario[6], usuario[7], lista_chaves, usuario[0])
+    return usuario
